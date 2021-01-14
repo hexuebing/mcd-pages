23824abb7e5ea10e49428fb60f25abfe947d45aa
hxb <839308443@qq.com>
2021-01-14 09:23:01 +0800

    update

--- bin/mcd-pages.js
+++ bin/mcd-pages.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
-function main(argv) {
-  console.info(argv)
-  console.info(process.cwd())
-  console.log('hello cli')
+const mcdPages = require('../lib/index')
+const program = require('commander');
+const path = require('path');
+const pkg = require('../package.json');
+
+function main(args) {
+  program
+  .version(pkg.version)
+  .option('-d, --dist <dist>', 'Base directory for all source files')
+  .option('-b, --branch <branch>', 'new branch for mcd', mcdPages.defaults.branch)
+  .parse(args);
+  console.info(program.dist)
+  console.info(program.branch)
 }
 
 if (require.main === module) {
